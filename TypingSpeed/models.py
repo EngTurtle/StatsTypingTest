3e26445de09559a25ace64a9396c56ab5b730404
Oliver Liang <ollie.liang@gmail.com>
2013-04-05 20:22:20 -0400

    allow null database values

--- TypingSpeed/models.py
+++ TypingSpeed/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
 class TestResult(models.Model):
-    total_time = models.DecimalField(max_digits=10, decimal_places=3)
-    errors = models.PositiveIntegerField()
-    device_string = models.CharField(max_length=120)
-    email = models.EmailField()
-    age = models.PositiveIntegerField()
+    total_time = models.DecimalField(max_digits=10, decimal_places=3, null=True)
+    errors = models.PositiveIntegerField(null=True)
+    device_string = models.CharField(max_length=120,null=True)
+    email = models.EmailField(null=True)
+    age = models.PositiveIntegerField(null=True)
+    gender = models.CharField(max_length=12,null=True)
 
     def get_absolute_url(self):
         return '/'
+
+    def __unicode__(self):
+        return u'Test by' + self.email
+
+admin.site.register(TestResult)
